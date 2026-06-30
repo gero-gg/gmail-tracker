14e9c4deae3e9bc4ba186c48fa17c8e5b8ee88c4
Gero <geronimogarciagutierrez@gmail.com>
2026-06-30 03:54:53 -0300

    Restauro imports y definición de app

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+import io
+import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+registros = []
+
 @app.route("/track/<email_id>")
 def track(email_id):
     ip_real = request.headers.get('X-Forwarded-For', request.remote_addr)
@@ -15,3 +25,10 @@ def track(email_id):
         0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59
     ])
     return send_file(io.BytesIO(pixel), mimetype="image/gif")
+
+@app.route("/registros")
+def ver_registros():
+    return {"registros": registros}
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
